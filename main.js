5a5d9d644c303e71ef7f7c34ebb49aaf64e24879
Sarah Hunt <shunt11@live.co.uk>
2024-12-03 17:39:03 +0000

    Add js content

---
+++ main.js
@@ -0,0 +1,28 @@
+const myCat = {
+    color: "black",
+    name: "Tony",
+    lovesCuddles: true,
+  };
+
+
+  const myHouse = {
+    numberOfStories: 2,
+    parking: false,
+    bedrooms: 2,
+    garden: true,
+    energyEfficiency: "D"
+  }
+
+  const myCar = {
+    color: red,
+    numberWheels: 4,
+    powerSteering: true,
+    numberSeats: 2,
+    milesToGallon: 2,
+     }  
+
+ const myFavouriteFilm = {
+    title = "The Dark Knight",
+    releaseDate = 2008,
+    leadActor = "Christian Bale",
+ }
